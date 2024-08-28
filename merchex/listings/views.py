9c5d7645601582c75c83522472d7774921832d25
hichem94 <hichem.eld@gmail.com>
2024-08-28 19:02:17 +0200

    migration

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band, Listing
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""<h1>Hello Django !</h1>
+                        <p>Mes groupes préférés sont :<p>
+                        <ul>
+                            <li>{bands[0].name}</li>
+                            <li>{bands[1].name}</li>
+                            <li>{bands[2].name}</li>
+                        </ul>                   
+                        """)
 
 
 def about(request):
@@ -10,7 +19,15 @@ def about(request):
 
 
 def listings(request):
-    return HttpResponse('<h1>On va lister les annonces pour les articles</h1>')
+    l = Listing.objects.all()
+    return HttpResponse(f"""<h1>On va lister les annonces pour les articles : </h1>
+                        <ul>
+                            <li>{l[0].title}</li>
+                            <li>{l[1].title}</li>
+                            <li>{l[2].title}</li>
+                            <li>{l[3].title}</li>
+                        </ul>  
+                        """)
 
 
 def contact(request):
