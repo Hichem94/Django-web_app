988e028fd1018be355cc31aa94b805ff1b5d2172
hichem94 <hichem.eld@gmail.com>
2024-09-01 13:08:34 +0200

    READ | Liens entre les pages

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,10 +1,45 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Band(models.Model):
+    
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+        RAP = 'RAP'  
+    
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=50)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=([MinValueValidator(1900), MaxValueValidator(2024)])
+    )        
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Listing(models.Model):
+    
+    class Type(models.TextChoices):
+        RECORD   = 'RECORD'
+        CLOTHING = 'CLOTHING'
+        POSTER   = 'POSTER'
+        DIVERS   = 'DIVERS'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        validators=([MinValueValidator(1900), MaxValueValidator(2024)])
+    )
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.title}'
 
 class Contact(models.Model):
     mail = models.fields.CharField(max_length=100)
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,20 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from listings.models import Band, Listing, Contact
+from django.shortcuts import get_object_or_404
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html', {'bands':bands})
+    return render(request, 
+                  'listings/band_list.html', 
+                  {'bands':bands})
+
+
+def band_detail(request, id):  # notez le paramètre id supplémentaire
+   #band = Band.objects.get(id=id)
+   band = get_object_or_404(Band, id=id)
+   return render(request,
+          'listings/band_detail.html',
+         {'band':band}) # nous passons l'id au modèle
 
 
 def about(request):
     return render(request, 'listings/about.html')
 
 
-def listings(request):
+def listings_list(request):
     l = Listing.objects.all()
-    return render(request, 'listings/listings.html', {'listings':l})
+    return render(request, 'listings/listings_list.html', {'listings':l})
 
+def listings_detail(request, id):
+    #l = Listing.get_objects.get(id=id)
+    l = get_object_or_404(Listing, id=id)
+    band = get_object_or_404(Band, id=id)
+    return render(request,
+                  'listings/listings_detail.html',
+                  {'listing':l, 'band':band})
 
 def contact(request):
     contact = Contact.objects.all()
